aed06c86e619f0d5a520d64d7024a1c907f5576b
picolah <159953868+picolah@users.noreply.github.com>
2024-12-01 05:45:05 +0000

    Update CLI.java

--- CLI.java
+++ CLI.java
@@ -9,22 +9,22 @@ public class CLI {
                 System.out.println("Please enter your role (Admin, HR, or User):");
                 String role = scanner.nextLine().trim();
 
-                // Admin Login
+                // admin Login
                 if ("Admin".equalsIgnoreCase(role)) {
                     AdminHandler.handleAdminLogin(scanner);
-                    break; // Exit the loop if Admin login is successful
+                    break; // exit the loop if admin login is successful
                 }
                 // HR Login
                 else if ("HR".equalsIgnoreCase(role)) {
                     HRHandler.handleHRLogin(scanner);
-                    break; // Exit the loop if HR login is successful
+                    break; // exit the loop if HR login is successful
                 }
-                // User Login
+                // user Login
                 else if ("User".equalsIgnoreCase(role)) {
                     UserHandler.handleUserLogin(scanner);
-                    break; // Exit the loop if User login is successful
+                    break; // exit the loop if User login is successful
                 }
-                // Invalid role entered
+                // invalid role entered
                 else {
                     System.out.println("Invalid role entered: Please try again!");
                 }

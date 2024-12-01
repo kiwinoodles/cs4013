c8b24978e09b4bced9861de13ccfe41f553fb383
picolah <159953868+picolah@users.noreply.github.com>
2024-12-01 05:46:03 +0000

    Update Payroll.java

--- Payroll.java
+++ Payroll.java
@@ -1,23 +1,24 @@
+package org.example;
+
+// Source code is decompiled from a .class file using FernFlower decompiler.
 import java.util.Calendar;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Payroll {
+    public Payroll() {
+    }
+
     public static void main(String[] args) {
         Timer timer = new Timer();
-        TimerTask promo = new TimerTask() {
-            public void run() {}
-        };
-        TimerTask pay = new TimerTask() {
-            public void run() {}
-        };
+        TimerTask promo = new Payroll$1();
+        TimerTask pay = new Payroll$2();
         Calendar promoDate = Calendar.getInstance();
-        promoDate.set(Calendar.MONTH,Calendar.OCTOBER);
-        promoDate.set(Calendar.DAY_OF_MONTH,1);
+        promoDate.set(2, 9);
+        promoDate.set(5, 1);
         timer.schedule(promo, promoDate.getTime());
         Calendar payDate = Calendar.getInstance();
-        promoDate.set(Calendar.DAY_OF_MONTH,25);
+        promoDate.set(5, 25);
         timer.schedule(pay, payDate.getTime());
     }
 }
-
